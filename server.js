8e9b80d35751044cad832ff89917411e51ca7c26
MithileshEN <87403588+MithileshEN@users.noreply.github.com>
2025-04-08 13:50:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-  const path = require('path');
+const path = require('path');
 
-  const app = express();
-  const PORT = 3000;
+const app = express();
+const PORT = 3000;
 
   // Serve static files from the public directory
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
   // Start the server
-  app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
